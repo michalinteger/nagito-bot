00f584a6df1860554fcb8f83d01d3b3225184e5e
MinecraftPlayYT <46090979+MinecraftPlayYT@users.noreply.github.com>
2020-06-23 21:55:03 +0200

    FIrst update

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+const path = require('path');    
+const filePath = path.join(__dirname, 'start.html');
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    if (bool(msg.content) === true) {
+        msg.reply('Pong!');
+    }
 });
 
 client.login('');
