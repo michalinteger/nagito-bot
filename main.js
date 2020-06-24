10d6ba5080407c9da2049a4a83fb552c6e250947
MinecraftPlayYT <46090979+MinecraftPlayYT@users.noreply.github.com>
2020-06-24 16:44:45 +0200

    Moved messaging to function

--- main.js
+++ main.js
@@ -5,36 +5,44 @@ const { token, spam } = require("./config/config.js");
 
 var boneMeme = "bone on the meat".split(" ");
 
+function sendMsg(text, ping = false, context = null) {
+    if (ping) {
+        msg.reply(text)
+    } else {
+        client.channels.cache.get(msg.channel.id).send(text)
+    }
+    if ( context != null ) {
+        console.log(context)
+    }
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     //client.channels.cache.get("725300214600171533").send("What is hope?");
 });
 
 client.on('message', msg => {
-    var noPingMsg = client.channels.cache.get(msg.channel.id);
-
     if (msg.author.bot) {
         return;
     } else if ( ( msg.mentions.users.has(client.user.id) || msg.mentions.users.has(client.user.tag) ) && msg.channel.type != "dm") {
         var cmd = msg.content.toLowerCase();
 
-        if ( cmd.includes("help") ) {
-            msg.reply("HOPE ".repeat(320).trim());
+        if ( cmd == `@${AbortController} info`) {
+            //reserved
+        } else if ( cmd.includes("help") ) {
+            sendMsg("HOPE ".repeat(320).trim(), ping = true);
         } else if ( cmd.includes("scary") ) {
-            msg.reply(client.user.avatarURL(options = {format: "png", size: 4096}));
+            sendMsg(client.user.avatarURL(options = {format: "png", size: 4096}), ping = true);
         } else if ( cmd.includes(boneMeme[0]) && cmd.includes(boneMeme[1]) && cmd.includes(boneMeme[2]) && cmd.includes(boneMeme[3]) ) {
-            msg.reply("MEAT ON THE BONE");
-            msg.reply("THE MEAT BONE ON");
-            msg.reply("ON THE MEAT BONE");
-            msg.reply("MEAT ON THE BONE");
+            sendMsg("\nMEAT ON THE BONE\nTHE MEAT BONE ON\nON THE MEAT BONE\nMEAT ON THE BONE", ping = true)
         } else {
-            msg.reply("Hope is the only way.");
+            sendMsg("Hope is the only way.", ping = true);
         }
 
     } else if (spam) {
-        noPingMsg.send("Hope is the only way.");
+        sendMsg("Hope is the only way.");
     } else if ( msg.channel.type == "dm" ) {
-        noPingMsg.send("Hope is the only way.");
+        sendMsg("Hope is the only way.");
     } else {
         return;
     }
