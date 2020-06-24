6d29bcc300457c59cb063350208f6312f6d0266f
MinecraftPlayYT <46090979+MinecraftPlayYT@users.noreply.github.com>
2020-06-24 13:49:36 +0200

    Added option to disable spam

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const { token } = require("./config/config.js");
+const { token, spam } = require("./config/config.js");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,10 +12,12 @@ client.on('message', msg => {
     if (msg.author.bot) {
         return
     } else {
-        if ( msg.mentions.users.has( client.user.id ) ) {
+        if ( msg.mentions.users.has(client.user.id) ) {
             msg.reply("Hope is the only way.");
         } else {
-            //client.channels.cache.get("725300214600171533").send("Hope is the only way.");
+            if (spam) {
+                client.channels.cache.get(msg.channel.id).send("Hope is the only way.");
+            }
         }
     }
 });
