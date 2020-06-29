a89cc3c8b1f7f53849e6be595be4ddf426ad2a6e
MinecraftPlayYT <46090979+MinecraftPlayYT@users.noreply.github.com>
2020-06-29 21:47:23 +0200

    Fixed spam variable

--- config/config.js
+++ config/config.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv").config()
 
-const token = process.env.token;
+var token = process.env.token;
+
 if (process.env.spam == "true") {
     var spam = true;
 } else {
     var spam = false;
 }
 
-var toExport = [token, spam]
-
-module.exports = {}
+module.exports = {
+    token: token,
+    spam: spam
+}
