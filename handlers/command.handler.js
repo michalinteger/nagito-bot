f5d893da407789bdd5650f3f7b3d2c716fed50e3
MinecraftPlayYT <46090979+MinecraftPlayYT@users.noreply.github.com>
2020-07-02 18:53:40 +0200

    Updated handler to export commands

--- handlers/command.handler.js
+++ handlers/command.handler.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 var commandList = fs.readdirSync("commands");
+var commands = new Object();
 
 for ( var commandNr = 0; commandNr < commandList.length; commandNr++ ) {
     var command = commandList[commandNr];
     if ( command.includes(".command.js") ) {
         console.log("var " + command.split(".")[0] + " = require(\"./command/" + command + "\")" + ";");
-        eval("var " + command.split(".")[0] + " = require(\"./command/" + command + "\")" + ";");
+        eval("var " + "commands" + command.split(".")[0] + " = require(\"./command/" + command + "\")" + ";");
     }
 }
 
-var a = info("aa");
+module.exports = commands;
